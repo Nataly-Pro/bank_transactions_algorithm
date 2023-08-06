85a0d7dd58b2d850567971fece4e0132e75822d1
natalya tomova <natalyayurevna19@gmail.com>
2023-08-06 23:47:35 +1000

    tested function get_operations_from_file

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,23 @@
-import pytest
 from src import utils
+from os import path
+
+
+def test_get_operations_from_file():
+    expected = [{'id': 441945886,
+             'state': 'EXECUTED',
+             'date': '2019-08-26T10:50:58.294041',
+             'description': 'Перевод организации',
+             'from': 'Maestro 1596837868705199',
+             'to': 'Счет 64686473678894779589'},
+             {'id': 41428829,
+             'state': 'EXECUTED',
+             'date': '2019-07-03T18:35:29.512364',
+             'description': 'Перевод организации',
+             'from': 'MasterCard 7158300734726758',
+             'to': 'Счет 35383033474447895560'}]
+
+    filename = path.join('.', 'file.json')
+    assert utils.get_operations_from_file(filename) == expected
 
 
 def test_sort_operations_by_date():
