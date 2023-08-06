acf71dd5165624619411d5c9047d7e445bddebc2
natalya tomova <natalyayurevna19@gmail.com>
2023-08-06 22:09:34 +1000

    tested function change_operation_date

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -42,5 +42,17 @@ def test_get_some_executed_operations():
                  "state": "EXECUTED"},
                 {"id": 649467725,
                  "state": "EXECUTED"}]
-
     assert utils.get_some_executed_operations(data) == expected
+
+
+def test_change_operation_date():
+    data = [{"id": 594226727,
+             "date": "2019-04-04T23:20:05.206878"},
+            {"id": 147815167,
+             "date": "2020-08-26T10:50:58.294041"}]
+
+    expected = [{"id": 594226727,
+                 "date": "4.4.2019"},
+                {"id": 147815167,
+                 "date": "26.8.2020"}]
+    assert utils.change_operation_date(data) == expected
