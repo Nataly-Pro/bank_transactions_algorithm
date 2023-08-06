f1689ca61b2dd3b54d8d8f308cca1ca2c64fef39
natalya tomova <natalyayurevna19@gmail.com>
2023-08-06 22:05:37 +1000

    tested function get_some_executed_operations

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,3 +14,33 @@ def test_sort_operations_by_date():
                  {"id": 594226727,
                  "date": "2019-04-04T23:20:05.206878"}]
     assert utils.sort_operations_by_date(data) == expected
+
+
+def test_get_some_executed_operations():
+    data = [{"id": 594226727,
+             "state": "CANCELED"},
+            {"id": 147815167,
+             "state": "EXECUTED"},
+            {"id": 716496732,
+             "state": "EXECUTED"},
+            {"id": 147815167,
+             "state": "EXECUTED"},
+            {"id": 518707726,
+             "state": "EXECUTED"},
+            {"id": 649467725,
+             "state": "EXECUTED"},
+            {"id": 782295999,
+             "state": "EXECUTED"}]
+
+    expected = [{"id": 147815167,
+                 "state": "EXECUTED"},
+                {"id": 716496732,
+                 "state": "EXECUTED"},
+                {"id": 147815167,
+                 "state": "EXECUTED"},
+                {"id": 518707726,
+                 "state": "EXECUTED"},
+                {"id": 649467725,
+                 "state": "EXECUTED"}]
+
+    assert utils.get_some_executed_operations(data) == expected
