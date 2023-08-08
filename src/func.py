b1b8da1552ba9e69e112459c6de67e6597542260
natalya tomova <natalyayurevna19@gmail.com>
2023-08-08 22:47:00 +1000

    added program file - func.py

---
+++ src/func.py
@@ -0,0 +1,23 @@
+from os import path
+from utils import *
+
+
+def get_operations_history(json_file):
+    operations = get_operations_from_file(json_file)
+    sorted_operations = sort_operations_by_date(operations)
+    executed_operations = get_some_executed_operations(sorted_operations)
+    formatted_date_operations = change_operation_date(executed_operations)
+
+    for operation in formatted_date_operations:
+        if operation["description"] != "Открытие вклада":
+            operation["from"] = mask_requisites(operation["from"])
+            operation["to"] = mask_requisites(operation["to"])
+        else:
+            operation["to"] = mask_requisites(operation["to"])
+
+    for operation in formatted_date_operations:
+        print(output_operations_history(operation))
+
+
+filename = '/home/natalya/PycharmProjects/course_work_project/src/operations.json'
+get_operations_history(filename)
