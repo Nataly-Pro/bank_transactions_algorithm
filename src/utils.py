1ca34260bf2071e4682128fb3b527a2634437ae3
natalya tomova <natalyayurevna19@gmail.com>
2023-08-10 22:09:33 +1000

    fixed functions: get_executed_operations, sort_operations_by_date, output_operations_history

--- src/utils.py
+++ src/utils.py
@@ -9,27 +9,23 @@ def get_operations_from_file(filename):
 
 def sort_operations_by_date(operations):
     """фильтрует массив от пустых операций и сортирует по убыванию даты операции"""
-    correct_operations = []
-    for operation in operations:
-        if operation:
-            correct_operations.append(operation)
-    sorted_operations = sorted(correct_operations,
-                               key=lambda correct_operations: correct_operations['date'],
+    sorted_operations = sorted(operations,
+                               key=lambda operations: operations['date'],
                                reverse=True)
-    return sorted_operations
+    return sorted_operations[:5]
 
 
-def get_some_executed_operations(operations, quantity=5):
+def get_executed_operations(operations):
     """принимает список словарей,
     возвращает массив, содержащий только исполненные операции
     в заданном количестве
     """
     executed_operations = []
     for operation in operations:
+        if not operation:
+            continue
         if operation["state"] == "EXECUTED":
             executed_operations.append(operation)
-        if len(executed_operations) == quantity:
-            break
     return executed_operations
 
 
