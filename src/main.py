b69a705d2bcaa806ef211a0ed47336f1100753ee
natalya tomova <natalyayurevna19@gmail.com>
2023-08-10 22:06:16 +1000

    changed main function return and order of utils use

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
-from utils import *
+from os import path
+from pathlib import Path
+from utils import get_operations_from_file, sort_operations_by_date,\
+    get_executed_operations, change_operation_date, mask_requisites,\
+    output_operations_history
 
+ROOT_PATH = Path(__file__).parent.parent
+SRC_PATH = Path.joinpath(ROOT_PATH, "src")
+OPERATION_JSON = Path.joinpath(SRC_PATH, "operations.json")
 
 def get_operations_history(json_file):
     operations = get_operations_from_file(json_file)
-    sorted_operations = sort_operations_by_date(operations)
-    executed_operations = get_some_executed_operations(sorted_operations)
-    formatted_date_operations = change_operation_date(executed_operations)
+    executed_operations = get_executed_operations(operations)
+    sorted_operations = sort_operations_by_date(executed_operations)
+    formatted_date_operations = change_operation_date(sorted_operations)
 
 # перебор операций в цикле для маскировки платежных реквизитов
     for operation in formatted_date_operations:
@@ -16,9 +23,10 @@ def get_operations_history(json_file):
             operation["to"] = mask_requisites(operation["to"])
 
 # применение функции форматированного вывода к каждой операции через цикл
+    history = []
     for operation in formatted_date_operations:
-        print(output_operations_history(operation))
+        history.append(output_operations_history(operation))
+    return '\n'.join(history)
 
 
-filename = '/home/natalya/PycharmProjects/course_work_project/src/operations.json'
-get_operations_history(filename)
+print(get_operations_history(OPERATION_JSON))
